e319666b189a6431d10a9682ba34d76ef4f2e6ab
t0dida00 <70305254+t0dida00@users.noreply.github.com>
2024-10-27 19:55:37 +0200

    update fectCache

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import TicketCard from "./(components)/TicketCard";
 
 const getTickets = async () => {
   try {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/Tickets/`, {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/Tickets`, {
       cache: "no-store",
     })
     return res.json()
