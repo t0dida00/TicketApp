2fb0f3f0efa102652afc20e3132dac7b8357dbcc
t0dida00 <70305254+t0dida00@users.noreply.github.com>
2024-10-27 19:35:38 +0200

    update API_URL

--- src/app/(components)/DeleteBlock.js
+++ src/app/(components)/DeleteBlock.js
@@ -7,7 +7,7 @@ import React from 'react'
 function DeleteBlock({ id }) {
     const router = useRouter()
     const deleteTicket = async () => {
-        const res = await fetch(`http://localhost:3000/api/Tickets/${id}`, {
+        const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/Tickets/${id}`, {
             method: "DELETE"
         })
         if (res.ok) {
